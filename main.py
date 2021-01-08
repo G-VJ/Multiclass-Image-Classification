e1d3b98c6a621ab3fd698fd75aa2a2d3890f2f18
gaurav-vijayvergiya <gauravvijayvergiya@gmail.com>
2021-01-08 11:26:42 +0530

    fix for heroku

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ async def predict_image(file: UploadFile = File(...)):
     
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = process.env.PORT
-    uvicorn.run("main:app", reload=True)
-    #uvicorn.run("main:app", reload=True, host="0.0.0.0", port=port)
+    port = int(process.env.PORT)
+    #uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=port)
