58cec72ddd0109491e5a3e76403af19a8958b6b0
gaurav-vijayvergiya <gauravvijayvergiya@gmail.com>
2021-01-18 13:30:46 +0530

    deployment changes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def hello():
     return {"message":"Hello World"}
 
 @app.post("/predict/image") 
-def predict_image(file: UploadFile = File(...)):
+async def predict_image(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
